940f3839e620993f74814d6ee217a55c2ccab798
Yunfan Li <lyf_liyunfan@sina.com>
2024-07-17 16:15:56 +0800

    P1

--- src/storage/access/ob_aggregated_store.cpp
+++ src/storage/access/ob_aggregated_store.cpp
@@ -452,7 +452,13 @@ int ObAggregatedStore::fill_count(const int64_t row_count)
     ret = OB_INVALID_ARGUMENT;
     STORAGE_LOG(WARN, "Invalid argument to fill count", K(ret), K(row_count));
   } else {
-    LOG_DEBUG("debug to fill row count", K(ret), K(row_count));
+    for (int64_t i = 0; OB_SUCC(ret) && i < agg_row_.get_agg_count(); ++i) {
+      ObAggCell *cell = agg_row_.at(i);
+      blocksstable::ObStorageDatum unused;
+      if (OB_FAIL(cell->eval(unused, row_count))) {
+        LOG_WARN("Failed to eval agg cell", K(ret), K(i), K(*cell));
+      }
+    }
   }
   return ret;
 }
